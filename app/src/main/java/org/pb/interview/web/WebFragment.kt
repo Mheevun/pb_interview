1bec223d99d74214b4d7051ecfe907bb5cbd6439
Mheevun <mheevun@gmail.com>
2017-02-22 19:10:03 +0700

    disable redirect to browser

--- app/src/main/java/org/pb/interview/web/WebFragment.kt
+++ app/src/main/java/org/pb/interview/web/WebFragment.kt
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import org.pb.interview.R
 import org.pb.interview.common.inflateBinding
 import org.pb.interview.databinding.FragmentWebBinding
@@ -18,6 +21,11 @@ class WebFragment(val url:String): Fragment(){
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = container!!.inflateBinding<FragmentWebBinding>(R.layout.fragment_web)
         binding.webview.loadUrl(url)
+        binding.webview.setWebViewClient(object:WebViewClient(){
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        })
         return binding.root
     }
 }
