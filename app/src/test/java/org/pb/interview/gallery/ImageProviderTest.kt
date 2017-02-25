8a265d297b0cd57119b798d8c953fee08e582bb5
Mheevun <pp.wasin@gmail.com>
2017-02-25 13:18:50 +0700

    prepare image provider

---
+++ app/src/test/java/org/pb/interview/gallery/ImageProviderTest.kt
@@ -0,0 +1,32 @@
+package org.pb.interview.gallery
+
+import com.cloudinary.Cloudinary
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by cnr on 25/2/2560.
+ */
+class ImageProviderTest{
+
+    lateinit var cloudinary:Cloudinary
+    lateinit var target:ImageProvider
+
+    @Before
+    fun setup(){
+        //TODO change to mock
+        val config = mutableMapOf<String,String>()
+        config.put("cloud_name", "ppwasin")
+        config.put("api_key", "776679255158782")
+        config.put("api_secret", "vXS7pgJ9Ge8fF6amdqMamOvmhjY")
+        cloudinary = Cloudinary(config)
+
+        target = ImageProvider(cloudinary,"test")
+    }
+
+    @Test
+    fun could_get_image_id_list(){
+        System.out.println("test")
+        val idList:List<String> = target.getImageIds()
+    }
+}
