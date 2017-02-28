7b5552734a534358d7465d298fdfdbabb3136977
Mheevun <mheevun@gmail.com>
2017-02-28 10:22:39 +0700

    delete non-used code

--- app/src/main/java/org/pb/interview/gallery/ImageAdapter.kt
+++ app/src/main/java/org/pb/interview/gallery/ImageAdapter.kt
@@ -41,15 +41,6 @@ class ImageAdapter (val fragmentHelper: FragmentHelper) : BaseAdapter() {
         return position.toLong()
     }
 
-    // Convert DP to PX
-    // Source: http://stackoverflow.com/a/8490361
-//    fun dpToPx(dps: Int): Int {
-//        val scale = context.resources.displayMetrics.density
-//        val pixels = (dps * scale + 0.5f).toInt()
-//
-//        return pixels
-//    }
-
     // create a new ImageView for each item referenced by the Adapter
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         Log.d(TAG, "position: $position")
@@ -85,28 +76,6 @@ class ImageAdapter (val fragmentHelper: FragmentHelper) : BaseAdapter() {
                     }
         }
     }
-
-
-    fun adjustImage(imageView:ImageView){
-
-        // Set height and width constraints for the image view
-//        imageView.layoutParams = LinearLayout.LayoutParams(wPixel, hPixel)
-
-        // Set the content of the image based on the provided URI
-//        imageView.setImageURI(
-//                Uri.withAppendedPath(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + imageID)
-//        )
-
-        // Image should be cropped towards the center
-        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-
-        // Set Padding for images
-        imageView.setPadding(8, 8, 8, 8)
-
-        // Crop the image to fit within its padding
-        imageView.cropToPadding = true
-    }
-
 
 
     class ViewHolder(var binding: GridItemBinding)
